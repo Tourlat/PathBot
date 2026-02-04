41637fe9915d5fd6d18565c87a6ec24bbdbebfb0
Tourlat <foucaultfelix33@gmail.com>
2026-02-04 13:38:13 +0100

    Fix deploy command on all guild

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('../config.json');
+const { clientId, token } = require('../config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -32,8 +32,8 @@ const rest = new REST().setToken(token);
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+        // The put method is used to fully refresh all commands globally
+        const data = await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
